f602c53750b2b450b0d3bbf1144874ff9963d235
leodelasoul <l.ricker93@web.de>
2017-01-10 17:33:33 +0100

    Add files via upload

---
+++ SeparateChainHashSet.java
@@ -0,0 +1,50 @@
+package javagit;
+
+import java.util.*;
+
+public class SeparateChainHashSet<E> {
+	
+	private int currentSize = 0;
+	private int m = 11;
+	private LinkedList<E>[] data;
+	
+	public SeparateChainHashSet(){
+		data = new LinkedList[m];
+		for(int i = 0; i < m; i++){
+			data[i] = new LinkedList<>();
+		}
+		
+	}
+	public boolean add(E e){
+		int hash = (e.hashCode() & 0x7ffffff) % m;
+		List<E> values = data[hash];
+		if (values.contains(e)){
+			return false;
+		}
+
+		values.add(e);
+		currentSize++;
+		return true;
+	}
+	
+	public boolean remove(Object o){
+		int hash = (o.hashCode() & 0x7ffffff) % m;
+		List<E> values = data[hash];
+		if(values.contains(o)){
+			values.remove(o);
+			currentSize--;
+			return true;
+		}
+		return false;
+	}
+	public boolean contains(Object o){
+		int hash = (o.hashCode() & 0x7ffffff) % m;
+		return data[hash].contains(0);
+
+	}
+	public int size(){
+		return currentSize;
+	}
+	
+
+}
